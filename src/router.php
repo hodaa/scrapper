f2782b8bbccfb7752c516701e09e423dd8932c21
hodamobile <hhussin@mobiledoctors24-7.com>
2018-07-08 13:26:36 +0200

    add factory to create object only one time

--- src/router.php
+++ src/router.php
@@ -1,29 +1,28 @@
 <?php
 
-use Scrapper\Controllers\Scrapper;
-use Scrapper\ScrapperProxy;
+use Scrapper\ScrapperFactory;
 
 $router = new \Bramus\Router\Router();
 
 $router->get('/', function () {
     $url = "https://dev98.de/";
-    $scrape = new Scrapper(new ScrapperProxy($url));
+    $scrape=ScrapperFactory::create($url);
     $scrape->index();
 });
 $router->get('/search', function () {
     $search = $_GET['s'];
     $url = "https://dev98.de/?s=" . $search;
-    $scrape = new Scrapper(new ScrapperProxy($url));
+    $scrape=ScrapperFactory::create($url);
     $scrape->index();
 });
 $router->get('/tag/(\w+)', function ($tag) {
     $url = "https://dev98.de/tag/" . $tag;
-    $scrape = new Scrapper(new ScrapperProxy($url));
+    $scrape=ScrapperFactory::create($url);
     $scrape->index();
 });
 $router->get('/more', function () {
     $url = $_GET['url'];
-    $scrape = new Scrapper(new ScrapperProxy($url));
+    $scrape=ScrapperFactory::create($url);
     $scrape->getDetails();
 });
 
